ee44b8afa0aa6414a280a5677dde1665798cf4db
Maestro1982 <kris.tote@telenet.be>
2023-10-31 15:31:34 +0100

    modify App.tsx in src folder, index.ts in src root pages folder, create AllUsers.tsx, CreatePost.tsx, EditPost.tsx, EditProfile.tsx, Explore.tsx, PostDetails.tsx, Profile.tsx and Saved.tsx in src root pages folder

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,17 @@ import SignInForm from './_auth/forms/SignInForm';
 import SignUpForm from './_auth/forms/SignUpForm';
 
 import RootLayout from './_root/RootLayout';
-import { Home } from './_root/pages';
+import {
+  AllUsers,
+  CreatePost,
+  EditPost,
+  EditProfile,
+  Explore,
+  Home,
+  PostDetails,
+  Profile,
+  Saved,
+} from './_root/pages';
 
 import { Toaster } from '@/components/ui/toaster';
 
@@ -26,6 +36,14 @@ const App = () => {
           {/* Private routes */}
           <Route element={<RootLayout />}>
             <Route index element={<Home />} />
+            <Route path='/explore' element={<Explore />} />
+            <Route path='/saved' element={<Saved />} />
+            <Route path='/all-users' element={<AllUsers />} />
+            <Route path='/create-post' element={<CreatePost />} />
+            <Route path='/update-post/:id' element={<EditPost />} />
+            <Route path='/posts/:id' element={<PostDetails />} />
+            <Route path='/profile/:id/*' element={<Profile />} />
+            <Route path='/update-profile/:id' element={<EditProfile />} />
           </Route>
         </Routes>
         <Toaster />
